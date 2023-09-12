d2d8189b23cafabceb8319bb87bac43de0cc8739
Nick <nickrray@gmail.com>
2023-09-11 22:36:14 -0500

    Extract import paths and display counts

--- import-analyzer.go
+++ import-analyzer.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func getImportPath(stm string) string {
+	var path string
+
+	fields := strings.Fields(stm)
+	for i, f := range fields {
+		if f == "from" {
+			path = fields[i+1]
+		}
+	}
+
+	if path == "" {
+		return path
+	}
+
+	var found bool
+	var r rune
+	start := 0
+	for !found && start < len(path) {
+		r = rune(path[start])
+		if !unicode.IsSpace(r) && r != '\'' && r != '"' {
+			found = true
+		} else {
+			start++
+		}
+	}
+
+	return path[start : strings.LastIndex(path, "/")+1]
+}
+
+func processStatement(paths *[]string, s string) {
+	if strings.Contains(s, "import") {
+		p := getImportPath(s)
+		if p != "" {
+			*paths = append(*paths, p)
+		}
+	}
+}
 
 func main() {
-    fmt.Println("Hello from import-analyzer!")
+	inputFile := flag.String("f", "", "Name of the file to use for input")
+	flag.Parse()
+
+	var paths []string
+
+	if *inputFile != "" {
+		// TODO: read file for content
+		fmt.Println("Provided file name: ", *inputFile)
+	} else {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			s := scanner.Text()
+			processStatement(&paths, s)
+		}
+	}
+
+	fmt.Println("Paths:")
+
+	occurrences := make(map[string]int)
+
+	for _, p := range paths {
+		_, ok := occurrences[p]
+		if !ok {
+			occurrences[p] = 0
+		}
+		occurrences[p] += 1
+	}
+
+	fmt.Println("Occurrences:")
+	for k, v := range occurrences {
+		fmt.Printf("[%s]: %d\n", k, v)
+	}
 }
